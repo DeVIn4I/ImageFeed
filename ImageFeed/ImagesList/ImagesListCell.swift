75c640ed24449375694a74e7cb7103b6ff059472
Razumov Pavel <i@dizzyfly.ru>
2022-11-19 14:22:20 +0300

    Добавил кастомную ячейку

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Pavel Razumov on 19.11.2022.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+}

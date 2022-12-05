ebf5a066429472bae8c3a8dca036743efe49dc40
Razumov Pavel <i@dizzyfly.ru>
2022-12-05 14:19:29 +0300

    Добавил IBOutlet IBAction

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,12 +1,19 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var profileNameLabel: UILabel!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var profileDescriptionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
+    @IBAction private func logOutButton() {
+        
+    }
 }

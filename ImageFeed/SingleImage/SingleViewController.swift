d8dc6c6325da0b0d760d3dc184e2c43267d27c07
Razumov Pavel <i@dizzyfly.ru>
2022-12-05 15:20:23 +0300

    Добавил новый экран для imageView. Добавил Navigator Bar

---
+++ ImageFeed/SingleImage/SingleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SingleViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Razumov on 05.12.2022.
+//
+
+import UIKit
+
+final class SingleViewController: UIViewController {
+
+    @IBOutlet private weak var fullImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

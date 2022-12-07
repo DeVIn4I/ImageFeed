81d7252f29f152a5dbc0601fac0ae62a776c2b64
Razumov Pavel <i@dizzyfly.ru>
2022-12-07 14:45:04 +0300

    Добавил метод scrollViewDidZoom для центрирования изображения после зума

--- ImageFeed/SingleImage/SingleViewController.swift
+++ ImageFeed/SingleImage/SingleViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class SingleViewController: UIViewController {
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var fullImageView: UIImageView!
     
@@ -25,13 +25,18 @@ final class SingleViewController: UIViewController {
         
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
-        
         fullImageView.image = image
         rescaleAndCenterImageInScrollView(image: image)
     }
     
     @IBAction private func didTapBackButton() {
         dismiss(animated: true)
+    }
+    
+    @IBAction private func didTapShareButton() {
+        let items = [image]
+        let ac = UIActivityViewController(activityItems: items as [Any], applicationActivities: nil)
+        present(ac, animated: true)
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
@@ -51,17 +56,17 @@ final class SingleViewController: UIViewController {
         let x = (newContentSize.width - visibleRectSize.width) / 2
         let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
-        
-
-        
-        
-        
     }
-    
 }
 
 extension SingleViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         fullImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0.0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0.0)
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
     }
 }

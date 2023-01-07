a068a70d07b5f43c085e1b4667d3fcb3ffda4379
Razumov Pavel <i@dizzyfly.ru>
2023-01-07 16:36:06 +0300

    Создал константы с ключами и URL

---
+++ ImageFeed/Constants/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "FWm9IBeWOQ6bMqZzuengryOeBcbUVI73_IQf2kJC574"
+let SecretKey = "QQyJ6kaOCAD6d2LwARfRBBHMh9kSRGK4lbFBPGS1_do"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")

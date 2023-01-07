038de626e92f2bfc4f634a99793a12a83889628a
Razumov Pavel <i@dizzyfly.ru>
2023-01-07 19:28:26 +0300

    Сверстал экран с входом в приложение. Создал экран с webView

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+import WebKit
+
+class WebViewViewController: UIViewController {
+    
+    @IBOutlet private weak var webView: WKWebView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        
+    }
+    
+}

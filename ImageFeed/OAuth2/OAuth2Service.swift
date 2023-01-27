834777e1ec728d42b565037ae65981c4fb809742
Razumov Pavel <i@dizzyfly.ru>
2023-01-27 13:36:02 +0300

    Переписал методы для URLSession.  Добавил их в Extension

--- ImageFeed/OAuth2/OAuth2Service.swift
+++ ImageFeed/OAuth2/OAuth2Service.swift
@@ -10,36 +10,20 @@ final class OAuth2Service {
     
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
         let request = authTokenRequest(code: code)
-        let task = object(for: request) { [weak self] result in
-            guard let self else { return }
+        urlSession.dataTask(type: OAuthTokenResponseBody.self, for: request) { [weak self] result in
             switch result {
             case .success(let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
+                self?.authToken = body.accessToken
+                completion(.success(body.accessToken))
             case .failure(let error):
                 completion(.failure(error))
             }
         }
-        task.resume()
     }
 }
 
 //MARK: OAuth2Servise
 extension OAuth2Service {
-    private func object(
-        for request: URLRequest,
-        completion: @escaping (Result<OAuthTokenResponseBody, Error>) -> Void
-    ) -> URLSessionTask {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return urlSession.data(for: request) { (result: Result<Data, Error>) in
-            let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
-                Result { try decoder.decode(OAuthTokenResponseBody.self, from: data) }
-            }
-            completion(response)
-        }
-    }
     
     private func authTokenRequest(code: String) -> URLRequest {
         URLRequest.makeHTTPRequest(
@@ -49,7 +33,7 @@ extension OAuth2Service {
             + "&&redirect_uri=\(redirectURI)"
             + "&&code=\(code)"
             + "&&grant_type=authorization_code",
-            httpMethod: "POST",
+            httpMethod: .POST,
             baseURL: URL(string: "https://unsplash.com")!
         )
     }
@@ -62,54 +46,6 @@ extension OAuth2Service {
     }
 }
 
-//MARK: HTTP Request
-extension URLRequest {
-    //    enum HTTPMethod: String { case GET, POST, DELETE, PUT }
-    static func makeHTTPRequest(
-        path: String,
-        httpMethod: String,
-        baseURL: URL = defaultBaseURL
-    ) -> URLRequest {
-        var request = URLRequest(url: URL(string: path, relativeTo: baseURL)!)
-        request.httpMethod = httpMethod
-        return request
-    }
-}
 
-//MARK: Network Connection
-enum NetworkError: Error {
-    case httpStatusCode(Int)
-    case urlRequestError(Error)
-    case urlSessionError
-}
 
-extension URLSession {
-    func data(
-        for request: URLRequest,
-        completion: @escaping (Result<Data, Error>) -> Void
-    ) -> URLSessionTask {
-        let fulfillCompletionOnTheMainThread: (Result<Data, Error>) -> Void = { result in
-            DispatchQueue.main.async {
-                completion(result)
-            }
-        }
-        
-        let task = dataTask(with: request) { data, response, error in
-            if let data = data,
-               let response = response,
-               let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                if 200..<300 ~= statusCode {
-                    fulfillCompletionOnTheMainThread(.success(data))
-                } else {
-                    fulfillCompletionOnTheMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
-                }
-            } else if let error = error {
-                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlRequestError(error)))
-            } else {
-                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlSessionError))
-            }
-        }
-        task.resume()
-        return task
-    }
-}
+
